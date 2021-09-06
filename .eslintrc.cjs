ba9d34e04baf8906b7a742adc20a462cb2a5ad73
maxwellstein <maxwell.stein@gmail.com>
2021-09-05 22:34:27 -0700

    initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier'],
+  root: true,
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# montrealsoundmap-api
+
+## API Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:8081
+$ yarn start:dev
+
+```
