fef66e13754b33a80ac4be9ed4b6ca4a91395994
Max Stein <maxwell.stein@gmail.com>
2024-07-18 16:16:12 -0700

    adds schema functions to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { randomBytes } from 'crypto'
 import slugify from 'slugify'
-import { ZodError } from 'zod'
+import { z, type RefinementCtx } from 'zod'
 
 export function generateItemIdFromMetadata(metadata: Record<string, string>) {
   const uuid = randomBytes(8).toString('hex').toLowerCase()
@@ -20,6 +20,31 @@ export function generateItemIdFromMetadata(metadata: Record<string, string>) {
   })
 }
 
-export function parseZodErrorToString(err: ZodError) {
-  return err.issues.map((issue, idx) => `(${idx + 1})${issue.path?.[0]} - ${issue.message}`).join(', ')
+export function isASCII(str: string): boolean {
+  // eslint-disable-next-line no-control-regex
+  return /^[\x00-\x7F]+$/.test(str)
+}
+
+export function parseZodErrorToString(err: z.ZodError) {
+  return err.issues.map((issue, _idx) => {
+    const path = issue.path?.[0] ? `${issue.path?.[0]} - ` : ''
+    return path + `${issue.message}`
+  }).join(', ')
+}
+
+/* https://github.com/colinhacks/zod/issues/61 */
+export function oneOf<A, K1 extends Extract<keyof A, string>, K2 extends Extract<keyof A, string>, R extends A &(
+  Required<Pick<A, K1>> & { [P in K2]: undefined } |
+  Required<Pick<A, K2>> & { [P in K1]: undefined }
+)>(key1: K1, key2: K2): ((arg: A, ctx: RefinementCtx) => arg is R) {
+  return (arg, ctx): arg is R => {
+    if ((arg[key1] === undefined) === (arg[key2] === undefined)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Either <${key1}> or <${key2}> must be set.`,
+      })
+      return false
+    }
+    return true
+  }
 }
