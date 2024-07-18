aa207d92686a34710f55bddbbdf33d949fbb9cad
Max Stein <maxwell.stein@gmail.com>
2024-07-18 16:15:03 -0700

    adds types and schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,41 @@
 import { z } from 'zod'
+import { oneOf } from './utils.js'
 
-export const IaOptions = z.object({
-  testmode: z.boolean().optional(),
-  setScanner: z.boolean().optional(),
+export const ia = {
+  Options: z.object({
+    testmode: z.boolean().optional(),
+    setScanner: z.boolean().optional(),
+  }),
+}
+
+export const Mediatype = z.enum(['audio', 'collection', 'data', 'etree', 'image', 'movies', 'software', 'texts', 'web'])
+
+export const UploadFileHeaders = z.object({
+  'x-archive-interactive-priority': z.number().optional(),
+  'x-archive-meta-mediatype': Mediatype,
+})
+
+export const CreateItemRequestHeaders = z.intersection(
+  z.object({
+    'authorization': z.string(),
+    'x-amz-auto-make-bucket': z.number(),
+    'x-archive-interactive-priority': z.number().optional(),
+    'x-archive-meta-identifier': z.string().or(z.number()),
+    'x-archive-meta-collection': z.string().or(z.number()).optional(),
+    'x-archive-meta01-collection': z.string().or(z.number()).optional(),
+    'x-archive-meta02-collection': z.string().or(z.number()).optional(),
+    'x-archive-meta-mediatype': Mediatype,
+  }).superRefine(oneOf('x-archive-meta-collection', 'x-archive-meta01-collection')),
+  z.record(z.string(), z.any()),
+)
+
+export const UpdateItemRequestPatch = z.object({
+  op: z.enum(['add']),
+  path: z.string(),
+  value: z.string(),
+})
+
+export const UpdateItemRequestData = z.object({
+  '-target': z.enum(['metadata']),
+  '-patch': z.array(UpdateItemRequestPatch),
 })
---
+++ src/types.ts
@@ -0,0 +1,93 @@
+import { z } from 'zod'
+import { ia, Mediatype, UpdateItemRequestData, UpdateItemRequestPatch, CreateItemRequestHeaders, UploadFileHeaders } from './schema.js'
+
+export type IaOptions = z.infer<typeof ia.Options>
+export type Mediatype = z.infer<typeof Mediatype>
+export type CreateItemRequestHeaders = z.infer<typeof CreateItemRequestHeaders>
+export type UpdateItemParams = Record<string, unknown>
+export type UpdateItemRequestPatch = z.infer<typeof UpdateItemRequestPatch>
+export type UpdateItemRequestData = z.infer<typeof UpdateItemRequestData>
+export type UploadFileHeaders = z.infer<typeof UploadFileHeaders>
+
+export interface CreateItemParams {
+  identifier: string
+  collection: string
+  mediatype: Mediatype
+  upload: FileUpload
+  metadata?: Record<string, unknown>
+}
+
+export interface CreateItemResponse {
+  identifier: string
+  metadata: Record<string, unknown>
+  upload: {
+    filename: string
+  }
+}
+
+export interface UpdateItemResponse {
+  success: boolean
+  error?: string
+  task_id?: number
+  log?: string
+}
+
+export interface GetItemsResponse {
+  responseHeader: {
+    status: number
+    QTtime: number
+    params: Record<string, unknown>
+  }
+  response: {
+    numFound: number
+    start: 0
+    docs: Record<string, unknown>[]
+  }
+}
+
+export interface GetItemResponse {
+  created: number
+  d1: string
+  d2: string
+  dir: string
+  files: Record<string, unknown>[]
+}
+
+export interface GetItemParams {
+  filters: {
+    collection?: string
+    subject?: string
+    creator?: string
+  }
+  options: {
+    fields?: string
+    rows?: string
+  }
+}
+
+export interface FileUpload {
+  filename: string
+  path?: string
+  data?: Buffer
+}
+
+export interface UploadFileParams {
+  identifier: string
+  mediatype: Mediatype
+  file: FileUpload
+}
+
+export interface GetItemTasksResponse {
+  succes: boolean
+  value: Record<string, unknown>
+}
+
+export interface TaskCriteria {
+  task_id?: number
+  server?: string
+  cmd?: string
+  args?: string
+  submitter?: string
+  priority?: number
+  wait_admin?: number
+}
