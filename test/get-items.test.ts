0ad04d462169a2f01a410ff1ccc43dd4ecd84739
mxwllstn <maxwell.stein@gmail.com>
2026-09-14 12:36:11 -0700

    chore: docs update

--- test/get-items.test.ts
+++ test/get-items.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import InternetArchive from '../dist'
+import InternetArchive from '../dist/index.js'
 const ia = new InternetArchive()
 
 test('get items', async () => {
